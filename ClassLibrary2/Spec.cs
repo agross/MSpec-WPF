a95c91c55929033c91ad6f09f527965a55152411
Alexander Groß <agross@therightstuff.de>
2013-03-12 18:12:17 +0100

    Initial commit

---
+++ ClassLibrary2/Spec.cs
@@ -0,0 +1,14 @@
+﻿using Machine.Specifications;
+
+namespace ClassLibrary2
+{
+  public class Spec
+  {
+    Establish context = () => { new UserControl1(); };
+
+    Because of = () => { new UserControl1(); };
+
+    It should_succeed =
+      () => true.ShouldBeTrue();
+  }
+}
